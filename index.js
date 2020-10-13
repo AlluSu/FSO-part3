339df389dea2a97ca62445cb87da58daaa8f2b9f
Aleksi Suuronen <aleksi.suuronen@outlook.com>
2020-10-13 19:43:09 +0300

    lisätty lint-työkalu

--- index.js
+++ index.js
@@ -13,125 +13,125 @@ mongoose.set('useFindAndModify', false)
 mongoose.set('useCreateIndex', true)
 
 let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-        id: 2
-    },
-    {
-        name: "Dan Abramov",
-        number: "12-43-234345",
-        id: 3
-    },
-    {
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-        id: 4
-    }
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4
+  }
 ]
 
 const getId = () => {
-    const max = Number.MAX_SAFE_INTEGER
-    const min = persons.length
-    const id = Math.floor(Math.random() * (max - min + 1)) + min
-    return id
+  const max = Number.MAX_SAFE_INTEGER
+  const min = persons.length
+  const id = Math.floor(Math.random() * (max - min + 1)) + min
+  return id
 }
 
 app.post('/api/persons', (request, response, next) => {
-    const personData = request.body
-    console.log(personData)
-    if (!personData.name) {
-        return response.status(400).json({
-            error : 'name missing'
-        })
-    }
-    if (!personData.number) {
-        return response.status(400).json({
-            error : 'number missing'
-        })
-    }
-    if (persons.find(taken => taken.name === personData.name)) {
-        return response.status(400).json({
-            error : 'name is already taken'
-        })
-    }
-
-    const person = new Person({
-        name: personData.name,
-        number: personData.number,
-        id: getId()
+  const personData = request.body
+  console.log(personData)
+  if (!personData.name) {
+    return response.status(400).json({
+      error : 'name missing'
     })
-
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
+  }
+  if (!personData.number) {
+    return response.status(400).json({
+      error : 'number missing'
     })
+  }
+  if (persons.find(taken => taken.name === personData.name)) {
+    return response.status(400).json({
+      error : 'name is already taken'
+    })
+  }
+
+  const person = new Person({
+    name: personData.name,
+    number: personData.number,
+    id: getId()
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
+  Person.findByIdAndRemove(request.params.id)
     .then(result => {
-        response.status(204).end()
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
-    })
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
     .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({}).then(person => {
-        response.json(person)
-    })
+  Person.find({}).then(person => {
+    response.json(person)
+  })
     .catch(error => next(error))
 })
 
 app.get('/info', (request, response, next) => {
-    const now = new Date()
-    const info = `<div>Phonebook has info for ${persons.length} people</div>`
-    response.write(info)
-    const timestamp = `<div>${now}</div>`
-    response.write(timestamp)
-    response.end()
+  const now = new Date()
+  const info = `<div>Phonebook has info for ${persons.length} people</div>`
+  response.write(info)
+  const timestamp = `<div>${now}</div>`
+  response.write(timestamp)
+  response.end()
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-    Person.findByIdAndUpdate(request.params.id, person, {new:true})
+  Person.findByIdAndUpdate(request.params.id, person, {new:true})
     .then(updatedPerson => {
-        response.json(updatedPerson)
+      response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-    if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    next(error)
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,45 +1,45 @@
-  const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-  if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
-  }
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
 
-  if (process.argv.length === 3) {
-    const password = process.argv[2]
-    const url =
+if (process.argv.length === 3) {
+  const password = process.argv[2]
+  const url =
       `mongodb+srv://Allu:${password}@cluster0.edo6b.mongodb.net/phonebook-app?retryWrites=true&w=majority`
-    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    const personSchema = new mongoose.Schema({
-      name: String,
-      number: String
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+  })
+  const Person = mongoose.model('Person', personSchema)
+  console.log('Phonebook:')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
-    const Person = mongoose.model('Person', personSchema)
-    console.log("Phonebook:")
-    Person.find({}).then(result => {
-      result.forEach(person => {
-        console.log(`${person.name} ${person.number}`)
-      })
-      mongoose.connection.close()
-    })
-  }
+    mongoose.connection.close()
+  })
+}
 
-  if (process.argv.length === 5) {
-    const password = process.argv[2]
-    const url =
+if (process.argv.length === 5) {
+  const password = process.argv[2]
+  const url =
       `mongodb+srv://Allu:${password}@cluster0.edo6b.mongodb.net/phonebook-app?retryWrites=true&w=majority`
-    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    const personSchema = new mongoose.Schema({
-      name: String,
-      number: String
-    })
-    const Person = mongoose.model('Person', personSchema)
-    const person = new Person({
-      name: process.argv[3],
-      number: process.argv[4]
-    })
-    person.save().then(response => {
-      console.log(`Added ${person.name} number ${person.number} to phonebook`)
-      mongoose.connection.close()
-    })
-  }
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+  })
+  const Person = mongoose.model('Person', personSchema)
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  person.save().then(response => {
+    console.log(`Added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
